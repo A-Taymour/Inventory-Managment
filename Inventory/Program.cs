1b24a44c6df30b652cdcbda47d6535b7c0256b7e
georgemarie <102537518+georgemarie@users.noreply.github.com>
2024-09-24 14:23:37 +0300

    performing clean architecture

--- Inventory/Program.cs
+++ Inventory/Program.cs
@@ -1,3 +1,6 @@
+using Inventory.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Inventory
 {
     public class Program
@@ -8,6 +11,9 @@ namespace Inventory
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDBcontext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
