7074a1aa35f0355c2841107b0d180d4c3beeb716
georgemarie <102537518+georgemarie@users.noreply.github.com>
2024-09-24 14:18:00 +0300

    adding Context

---
+++ Inventory.DB/Data/ApplicationDBcontext.cs
@@ -0,0 +1,20 @@
+﻿using Inventory.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Data
+{
+    public class ApplicationDBcontext : DbContext
+    {
+        public ApplicationDBcontext(DbContextOptions<ApplicationDBcontext> options)
+       : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Alert> Alerts { get; set; }
+
+    }
+}
