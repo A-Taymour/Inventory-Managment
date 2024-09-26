e2aa7db92c64774e03403d8ead697958f4953b46
georgemarie <102537518+georgemarie@users.noreply.github.com>
2024-09-26 16:54:48 +0300

    modyfing Models

--- Inventory.DB/Models/User.cs
+++ Inventory.DB/Models/User.cs
@@ -12,15 +12,13 @@ public class User
     [PasswordPropertyText]
     [Required]
     public string Password { get; set; }
-    public bool Admin { get; set; }
-    public bool Supplier { get; set; }
-    public bool Client { get; set; }
     [EmailAddress]
     [Required]
     public string Email { get; set; }
     [StringLength(11, MinimumLength = 11, ErrorMessage = "phone number must be 11")]
     [Required]
     public string Phone { get; set; }
+    public bool IsAdmin{ get; set; }
 
     public ICollection<Product> Products { get; set; }
     public ICollection<Transaction> Transactions { get; set; }
